a894410292c0b9e0490a47efbf9fcbaeea47aea6
AN3223 <35916076+AN3223@users.noreply.github.com>
2019-02-20 12:43:38 -0600

    Opens a window now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+extern crate gl;
+extern crate glutin;
+
+use glutin::dpi::LogicalSize;
+use glutin::GlContext;
+
+fn handle_keypress(input: glutin::KeyboardInput) {
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut events_loop = glutin::EventsLoop::new();
+    let window = glutin::WindowBuilder::new()
+        .with_title("Hello, world!")
+        .with_dimensions(LogicalSize::new(500.0, 300.0));
+    let context = glutin::ContextBuilder::new();
+    let gl_window = glutin::GlWindow::new(window, context, &events_loop).unwrap();
+
+    unsafe {
+        gl_window.make_current().unwrap();
+        gl::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _);
+        gl::ClearColor(1.0, 1.0, 1.0, 1.0);
+    }
+
+    let mut running = true;
+    while running {
+        events_loop.poll_events(|event| {
+            match event {
+                glutin::Event::WindowEvent{ event, .. } => match event {
+                    glutin::WindowEvent::KeyboardInput { input, .. } => {
+                        handle_keypress(input);
+                    }
+                    glutin::WindowEvent::CloseRequested => running = false,
+                    _ => ()
+                },
+                _ => ()
+            }
+        });
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        gl_window.swap_buffers().unwrap();
+    }
 }
