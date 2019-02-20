3091b820f16af06a097df96eec6ddff756b65cea
AN3223 <35916076+AN3223@users.noreply.github.com>
2019-02-20 05:46:06 -0600

    Initial commit

---
+++ build.rs
@@ -0,0 +1,36 @@
+extern crate reqwest;
+extern crate rayon;
+
+use rayon::iter::ParallelBridge;
+use rayon::prelude::*;
+
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::io;
+use std::fs;
+
+fn get_sound_files() {
+    let handle = File::open("piano_sound_links")
+        .expect("unable to find piano_sound_links");
+
+    // Concurrently iterate over all of the links in piano_sound_links
+    // and download them
+    BufReader::new(handle).lines().par_bridge().map(
+        |linkr| {
+            let link = linkr.expect("failed to read from piano_sound_links");
+            let mut resp = reqwest::get(&link).expect("request failed");
+
+            let filename = "sounds/".to_owned() + &link.clone().split_off(link.len() - 7);
+            let mut file = File::create(filename).expect("file creation failed");
+
+            io::copy(&mut resp, &mut file).expect("failed to copy to file");
+        }
+    ).for_each(drop);
+}
+
+fn main() {
+    if !fs::metadata("sounds").is_ok() {
+        fs::create_dir("sounds").expect("could not create sounds directory");
+        get_sound_files();
+    }
+}
